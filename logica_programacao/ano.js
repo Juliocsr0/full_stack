5d5a9da78f6baf9ad321d65f58dc9ee73b7712d5
Juliocsr0 <juliocesarmax01@gmail.com>
2025-11-10 16:15:58 -0300

    Descobrindo ano de nascimento do usuário

---
+++ logica_programacao/ano.js
@@ -0,0 +1,14 @@
+// Criar um programa, para verificar o ano de nascimento
+const anoAtual = 2025;
+const idadeUsuario = Number(prompt("Qual é a sua idade?"));
+const anoNascimento = anoAtual - idadeUsuario;
+
+console.log("Seu ano de nascimento é:", anoNascimento);
+
+// Diego, 35 anos
+// Juliana, 10 anos
+// João, 17 anos
+
+console.log("Diego nasceu em:", 2025 - 35);
+console.log("Juliana nasceu em:", 2025 - 10);
+console.log("João nasceu em:", 2025 - 17);
