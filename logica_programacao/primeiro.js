fef5895bcc6fb5c645675a0e8f021106d786133b
Juliocsr0 <juliocesarmax01@gmail.com>
2025-11-10 15:13:00 -0300

    Adicionando arquivos JS de lógica de programação

---
+++ logica_programacao/primeiro.js
@@ -0,0 +1,11 @@
+// Perguntando o namo para o usuário
+const userName = prompt("Qual é o seu nome?");
+
+// Perguntando a idade do usuário
+const usarAge = prompt("Qual é a sua idade?");
+
+// Imprimindo na tela o resultado
+document.write("Seu nome é: " + "<strong>" + userName + "</strong>" + "<br>");
+
+// Imprimindo na tela o resultado da idade
+document.write("<br>Sua idade é: " + "<strong>" + usarAge + "</strong>" + " anos");
---
+++ logica_programacao/segundo.js
@@ -0,0 +1,4 @@
+let numero1 = prompt("Digite um número:");
+let numero2 = prompt("Digite outro número:");
+
+console.log("numero 2"+ "numero1");
