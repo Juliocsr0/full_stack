ef57c11a73edb5095ff0ba82e18d633063e48e33
Juliocsr0 <juliocesarmax01@gmail.com>
2025-11-19 14:06:52 -0300

    Colocando a lista de contatos

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,11 +1,34 @@
+const form = document.querySelector("form");
+const lista = document.querySelector(".lista");
 const inputNome = document.getElementById("nome");
-const inputEmail = document.getElementById("email")
-const inputTel = document.getElementById("telefone")
+const inputEmail = document.getElementById("email");
+const inputTel = document.getElementById("telefone");
 
-function btnCadastrar(event) {
-  // alert('Você clicou no botão de cadastrar');
-  event.preventDefault();
-  console.log(inputNome.value);
-  console.log(inputEmail.value);
-  console.log(inputTel.value);
-}
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Validação do formulário
+    if (inputNome.value == "" || inputEmail.value == "" || inputTel.value == "") {
+        alert("Digite seus dados");
+        return false;
+    }
+
+    // Criar LI
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+        <span class="contato-nome">${inputNome.value}</span>
+        <span class="contato-email">${inputEmail.value}</span>
+        <span class="contato-telefone">${inputTel.value}</span>
+    `;
+
+    console.log(li)
+
+
+    // appendChild()
+    lista.appendChild(li)
+
+    // Limpar inputs
+    form.reset();
+
+})
