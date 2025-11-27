1240c1ccb6033b2d8a172e0ea38fccb40069768f
Juliocsr0 <juliocesarmax01@gmail.com>
2025-11-27 16:24:47 -0300

    Adicionando rotas para variável times

--- node-people/index.js
+++ node-people/index.js
@@ -2,6 +2,9 @@ const express = require ("express");
 const app= express();
 const port = 3000;
 
+// Indicar para express ler body com json
+app.use(express.json());
+
 // mock
 const nomes = [
   { id: 1, nome: "Fernanda", idade: "18" },
@@ -11,23 +14,42 @@ const nomes = [
   { id: 5, nome: "Doris", idade: "33" },
 ];
 
-// Criando funções auxiliares
-// Retornar o objeto por id
+const times = [
+  { id: 1, nome: "Corinthians", estado: "SP", titulos: 7 },
+  { id: 2, nome: "Palmeiras", estado: "SP", titulos: 11 },
+  { id: 3, nome: "Santos", estado: "SP", titulos: 8 },
+  { id: 4, nome: "Flamengo", estado: "RJ", titulos: 7 },
+  { id: 5, nome: "Vasco", estado: "RJ", titulos: 4 },
+  { id: 6, nome: "Atlético Mineiro", estado: "MG", titulos: 3 },
+  { id: 7, nome: "Cruzeiro", estado: "MG", titulos: 4 },
+];
 
+// Criando funções auxiliares para o time
+// Retornar o objeto por id
+function buscarTimePorId(id) {
+    return times.filter((time) => time.id == id)
+};
+
+function buscarIdTime(id) {
+    return times.findIndex((time) => time.id == id)
+};
+
+//---------------------------------------------------------------
+
+// Criando funções auxiliares para nomes
+// Retornar o objeto por id
 function buscarNomePorId(id) {
     return nomes.filter((nome) => nome.id == id)
-
-}
+};
 
 // Pegar a posição ou index do elemento do Arrey por id
-
 function buscarIdNomes(id) {
     return nomes.findIndex((nome) => nome.id == id);
-}
+};
 
 app.listen (port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-})
+});
 
 // Rota principal
 app.get("/", (req, res) => {
@@ -45,9 +67,9 @@ app.get("/listanomes", (req, res) => {
 });
 
 // Criando Post para cadastrar
-app.post("/listanomes", (res, req) => {
+app.post("/listanomes", (req, res) => {
     nomes.push(req.body);
-    res.status(201).send('Nome cadastrado com sucesso!')
+    res.status(201).send('Nome cadastrado com sucesso!');
 });
 
 // Criando rota excluir
@@ -66,3 +88,30 @@ app.get("/listanomes/:id", (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
+//-----------------------------------------------------------------------
+
+// Buscando times (lista times)
+
+app.get("/listatimes", (req, res) => {
+    res.json(times);
+});
+
+// Criando Post para cadastrar (times)
+app.post("/listatimes", (req, res) => {
+    times.push(req.body);
+    res.status(201).send('Time cadastrado com sucesso!');
+});
+
+// Criando rota excluir (times)
+app.delete("/listatimes/:id", (req, res) => {
+    let index = buscarIdTime(req.params.id);
+    times.splice(index, 1);
+    res.send(`Time com id ${req.params.id} excluída com sucesso!`);
+});
+
+// Buscando por id (times)
+app.get("/listatimes/:id", (req, res) => {
+    let index = req.params.id;
+    res.json(buscarTimePorId(index));
+})
